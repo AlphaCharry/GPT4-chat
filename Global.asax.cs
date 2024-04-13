e3d1dee1b94feea24876ad99caa95244141200cf
BlackPanda <38164385+AlphaCharry@users.noreply.github.com>
2024-04-13 11:42:12 +0800

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using PurpleWind.Code;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PurpleWind.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// 启动应用程序
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
